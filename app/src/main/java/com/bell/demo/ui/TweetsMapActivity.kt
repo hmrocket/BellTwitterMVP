cd1953c49c8c696e3aec75d00010caa12507c6e6
M'hamed HLIOUI <hmrocket@gmail.com>
2019-06-26 20:01:47 -0400

    add radius preference

--- app/src/main/java/com/bell/demo/ui/TweetsMapActivity.kt
+++ app/src/main/java/com/bell/demo/ui/TweetsMapActivity.kt
@@ -8,19 +8,22 @@ import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.SeekBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.bell.demo.R
-import com.bell.demo.ui.search.SearchActivity
+import com.bell.demo.model.TweetType
 import com.bell.demo.utils.Utils
+import com.bell.demo.utils.visible
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import com.squareup.picasso.Picasso
 import com.twitter.sdk.android.core.Callback
 import com.twitter.sdk.android.core.Result
@@ -30,8 +33,11 @@ import com.twitter.sdk.android.core.models.Coordinates
 import com.twitter.sdk.android.core.models.Search
 import com.twitter.sdk.android.core.models.Tweet
 import com.twitter.sdk.android.core.services.params.Geocode
+import kotlinx.android.synthetic.main.activity_tweets_map.*
 import kotlinx.android.synthetic.main.map_tweet.view.*
 import java.util.*
+import kotlin.math.ln
+import kotlin.math.roundToInt
 
 class TweetsMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
     GoogleMap.OnMyLocationClickListener {
@@ -47,6 +53,7 @@ class TweetsMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnM
     private var queue: List<Tweet> = Collections.emptyList()
     private var location: Location? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tweets_map)
@@ -58,10 +65,16 @@ class TweetsMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnM
         // get location
         getCurrentLocation()
 
-        // load tweets
+        fetchGeoTweets(10)
+    }
+
+    private fun fetchGeoTweets(radius: Int) {
+        // load tweets if location is null take montreal
         val geocode =
-            Geocode(location?.latitude ?: 45.5017, location?.longitude ?: 73.5673,
-                9995, Geocode.Distance.KILOMETERS)
+            Geocode(
+                location?.latitude ?: 45.5017, location?.longitude ?: 73.5673,
+                radius, Geocode.Distance.KILOMETERS
+            )
         TwitterCore.getInstance().apiClient.searchService.tweets(
             "#food", geocode, null,
             null, null, 100, null, null, null, null
@@ -77,6 +90,19 @@ class TweetsMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnM
             }
 
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_map, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (R.id.action_radius == item.itemId) {
+            seekBar_radius.visible(!seekBar_radius.isVisible) // toggle visibility
+            true
+        } else
+            super.onOptionsItemSelected(item)
     }
 
     @SuppressLint("MissingPermission")
@@ -95,6 +121,11 @@ class TweetsMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnM
         if (queue.isEmpty() || !mapReady) {
             return
         }
+        // remove all old markers
+        mMap.clear()
+        addCircle()
+
+
         queue.forEachIndexed { i, it ->
             run {
                 val pos: LatLng? = it.coordinates?.let { it2 -> LatLng(it2.latitude, it2.longitude) }
@@ -111,11 +142,13 @@ class TweetsMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnM
 
                 // if we have a non null position add marker
                 pos?.let { it2 ->
-                    val text = "${it.user.profileImageUrlHttps}~${it.createdAt}~${it.text}~$i"
+                    val text = InfoAdapter.encodeTweetInfo(it)
                     mMap.addMarker(MarkerOptions().position(it2).title(it.user.name).snippet(text))
                 }
             }
         }
+
+        seekBar_radius.visible(false)
     }
 
     /**
@@ -133,18 +166,65 @@ class TweetsMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnM
         mapReady = true
 
         // move the camera to current position
-        val currentLocation = LatLng(45.5017, 73.5673)
+        val currentLocation =
+            location?.let { LatLng(location!!.latitude, location!!.longitude) } ?: LatLng(45.5017, 73.5673)
         mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
         //val location = CameraUpdateFactory.newLatLngZoom(currentLocation, 12.0f)
 
         val adapter = InfoAdapter(this)
         mMap.setInfoWindowAdapter(adapter)
         mMap.setOnInfoWindowClickListener(adapter)
-//        mMap.animateCamera(location)
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 12.0f))
         mMap.isMyLocationEnabled = true
 
         displayTweets()
+
+        addCircle()
+
     }
+
+    private fun addCircle() {
+        val currentLocation = location?.let { LatLng(location!!.latitude, location!!.longitude) }
+            ?: LatLng(45.5017, 73.5673)
+
+        val radius = 5000.0 // min
+        val circle: Circle = mMap.addCircle(
+            CircleOptions()
+                .center(currentLocation)
+                .radius(radius) // Converting Miles into Meters...
+                .strokeColor(getColor(R.color.colorAccent))
+                .strokeWidth(2f)
+        )
+
+        seekBar_radius.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                circle.radius = p1.toDouble()
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, getZoomLevel(circle)))
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                circle.isVisible = true
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                // circle.isVisible = false
+                fetchGeoTweets((circle.radius / 1000).toInt())
+
+            }
+
+        })
+    }
+
+    // Credit: https://stackoverflow.com/questions/39386140/zooming-google-map-to-specific-radius-in-miles-in-android
+    private fun getZoomLevel(circle: Circle?): Float {
+        if (circle == null) return 0.5f
+
+        val radius = circle.radius
+        val scale = radius / 500
+        val zoomLevel = (16 - ln(scale) / ln(2.0)).roundToInt().toFloat()
+        return zoomLevel - 0.5f
+    }
+
 
     override fun onMyLocationButtonClick(): Boolean {
         Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show()
@@ -156,18 +236,35 @@ class TweetsMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnM
     }
 
     class InfoAdapter(private val context: Context) : GoogleMap.InfoWindowAdapter, GoogleMap.OnInfoWindowClickListener {
-        override fun onInfoWindowClick(p0: Marker?) {
-            SearchActivity.launch(context)
+
+        companion object {
+            fun parseTweetInfo(marker: Marker): List<String> = marker.snippet.split("~")
+            fun encodeTweetInfo(it: Tweet): String =
+                "${it.user.profileImageUrlHttps}~${it.createdAt}~${it.text}~${it.id}~${TweetType.getType(it)}"
+
+            const val INDEX_PHOTO_URL = 0
+            const val INDEX_TIME = 1
+            const val INDEX_TWEET_TEXT = 2
+            const val INDEX_TWEET_ID = 3
+            const val INDEX_TWEET_TYPE = 4
+        }
+
+        override fun onInfoWindowClick(p0: Marker) {
+            val text = parseTweetInfo(p0)
+            val id = text[INDEX_TWEET_ID].toLong()
+            val type = text[INDEX_TWEET_TYPE]
+
+            TweetActivity.launch(context, id, TweetType.valueOf(type))
         }
 
         override fun getInfoContents(marker: Marker): View {
             val v = LayoutInflater.from(context).inflate(R.layout.map_tweet, null)
 
-            val text = marker.snippet.split("~")
+            val text = parseTweetInfo(marker)
 
-            val photoUrl = text[0]
-            val timestamp = text[1]
-            val tweet = text[2]
+            val photoUrl = text[INDEX_PHOTO_URL]
+            val timestamp = text[INDEX_TIME]
+            val tweet = text[INDEX_TWEET_TEXT]
 
             Picasso.with(context)
                 .load(photoUrl)
--- app/src/main/java/com/bell/demo/ui/search/holders/StatusLinkViewHolder.kt
+++ app/src/main/java/com/bell/demo/ui/search/holders/StatusLinkViewHolder.kt
@@ -2,57 +2,9 @@ package com.bell.demo.ui.search.holders
 
 import android.view.View
 import com.bell.demo.ui.search.InteractionListener
-import com.twitter.sdk.android.core.models.Tweet
 
 /**
- *
+ * TODO work on link preview
  */
 class StatusLinkViewHolder(container: View, listener: InteractionListener) :
-        StatusViewHolder(container, listener) {
-
-//    private val urlPreviewImageView: ImageView = container.urlPreviewImageView
-//    private val urlTitleTextView: TextView = container.urlTitleTextView
-//    private val urlTextDescriptionView: TextView = container.urlDescriptionTextView
-//    private val urlPreviewLayout = container.urlPreviewLayout
-//    private val loadingProgressBar = container.loadingProgressBar
-//    private var disposable: Disposable? = null
-
-    override fun setup(tweet: Tweet) {
-        super.setup(tweet)
-
-//        setLoading(true)
-//        if (tweet.metaData == null) {
-//            urlPreviewLayout.setOnClickListener { }
-//            disposable?.dispose()
-//            disposable = UrlInfo.generatePreview(tweet.getLink())
-//                    .subscribeOn(Schedulers.io())
-//                    .observeOn(AndroidSchedulers.mainThread())
-//                    .subscribe({
-//                        run {
-//                            tweet.metaData =
-//                                    MetaData(if (it.images.isNotEmpty()) it.images[0].source
-//                                    else null, it.title, it.description, tweet.getLink())
-//                            loadPreview(tweet.metaData as MetaData)
-//                        }
-//                    }, { e -> Timber.e(e, "Error loading url preview") })
-//        } else loadPreview(tweet.metaData as MetaData)
-    }
-
-//    private fun setLoading(isLoading: Boolean) {
-//        loadingProgressBar.visible(isLoading)
-//        urlPreviewImageView.visible(!isLoading)
-//        urlTitleTextView.visible(!isLoading)
-//        urlTextDescriptionView.visible(!isLoading)
-//    }
-
-//    private fun loadPreview(metaData: MetaData) {
-//        setLoading(false)
-//        if (metaData.imageUrl != null)
-//            urlPreviewImageView.loadUrl(metaData.imageUrl)
-//        else
-//            urlPreviewImageView.visibility = View.GONE
-//        urlTitleTextView.text = metaData.title
-//        urlTextDescriptionView.text = metaData.description
-//        urlPreviewLayout.setOnClickListener { openUrl(container.context as Activity, metaData.link) }
-//    }
-}
+        StatusViewHolder(container, listener)
